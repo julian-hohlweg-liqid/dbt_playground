35bf7b16ad211c17232eefee45978fab7c753290
mirsad.sukilovic@liqid.de <mirsad.sukilovic@liqid.de>
2022-12-26 14:40:39 +0000

    left join fixed

--- models/SignedDatabase.sql
+++ models/SignedDatabase.sql
@@ -14,7 +14,18 @@ with contract as (
         InvestmentType__c
     from `third-being-207111.RAW.SF_CONTRACT`
     where RecordTypeId != "0122X000000or7uQAA"
+),
+
+docusign as (
+    select 
+        dsfs__Contract__c,
+        dsfs__Envelope_Status__c,
+        MIN((CAST(dsfs__Completed_Date_Time__c as DATE))) as dsfs__Completed_Date_Time__c
+    from `third-being-207111.RAW.SF_DOCUSIGN_STATUS`
+    where dsfs__Envelope_Status__c = "Completed"
+    group by dsfs__Envelope_Status__c, dsfs__Contract__c
 )
 
 select *
-from contract
+from contract left join docusign
+on contract.Contract_Id = docusign.dsfs__Contract__c
