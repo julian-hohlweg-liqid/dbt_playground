7068ea7992cc7df8c5e22cc1b810f033a62918c0
mirsad.sukilovic@liqid.de <mirsad.sukilovic@liqid.de>
2022-12-28 12:48:22 +0000

    mail != @liqid

--- models/Signed_Database.sql
+++ models/Signed_Database.sql
@@ -1,36 +1,50 @@
-{{ config(materialized="table") }}
+{{ config(materialized='table') }}
 
 with contract as (
     select 
         Contact__c as Contact_Id,
         Id as Contract_Id,
-        CAST(CustomerSignedDate as DATE) as CustomerSignedDate,
+        cast(CustomerSignedDate as date) as CustomerSignedDate,
         RecordTypeId,
-        CAST(peo_portfolio_commitment_amount__c as NUMERIC) as peo_portfolio_commitment_amount__c,
+        cast(peo_portfolio_commitment_amount__c as numeric) as peo_portfolio_commitment_amount__c,
         peo_portfolio_product__c,
         InvestmentStrategy__c,
         RiskLevel__c,
-        CAST(InvestmentAmount__c as NUMERIC) as InvestmentAmount__c,
+        cast(InvestmentAmount__c as numeric) as InvestmentAmount__c,
         InvestmentType__c
     from `third-being-207111.RAW.SF_CONTRACT`
-    where RecordTypeId != "0122X000000or7uQAA"
+    where RecordTypeId != '0122X000000or7uQAA'
 ),
 
 docusign as (
     select 
         dsfs__Contract__c,
         dsfs__Envelope_Status__c,
-        MIN((CAST(dsfs__Completed_Date_Time__c as DATE))) as dsfs__Completed_Date_Time__c
+        min((cast(dsfs__Completed_Date_Time__c as date))) as dsfs__Completed_Date_Time__c
     from `third-being-207111.RAW.SF_DOCUSIGN_STATUS`
-    where dsfs__Envelope_Status__c = "Completed"
+    where dsfs__Envelope_Status__c = 'Completed'
     group by dsfs__Envelope_Status__c, dsfs__Contract__c
 ),
 
-joined_table as (
+contact as (
+    select
+        Id,
+        Email
+    from `third-being-207111.RAW.SF_CONTACT`
+),
+
+joined_table_1 as (
     select *
     from contract
     left join docusign
     on contract.Contract_Id = docusign.dsfs__Contract__c
+),
+
+joined_table_2 as (
+    select *
+    from joined_table_1
+    left join contact
+    on joined_table_1.Contact_Id = contact.Id
 )
 
 select
@@ -51,5 +65,6 @@ select
         when RecordTypeId = '0122X000000orDeQAI' then CustomerSignedDate
         when RecordTypeId = '0127R000000tY5tQAE' then dsfs__Completed_Date_Time__c
     end) as Signature_Date
-from joined_table
+from joined_table_2
+where not (Email LIKE '%@liqid%')
 
