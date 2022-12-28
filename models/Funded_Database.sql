7d74f4fd2bf5366fd92331a22f0c541ec5db052c
mirsad.sukilovic@liqid.de <mirsad.sukilovic@liqid.de>
2022-12-28 13:43:05 +0000

    funded database initial

--- models/Funded_Database.sql
+++ models/Funded_Database.sql
@@ -1,2 +1,29 @@
-{{ config(materialized="table") }}
+{{ config(materialized = 'table') }}
 
+with transactions as (
+    select
+        Id,
+        Type__c,
+        PortfolioRef__c as Portfolio_Id,
+        cast(SettlementDate__c as date) as Transaction_Date,
+        Amount__c as Amount
+    from `third-being-207111.RAW.SF_TRANSACTION`
+    where
+        Type__c = 'Deposit'
+        and Amount__c is not null
+),
+
+portfolio as (
+    select
+        Id,
+        RecordTypeId,
+        Contract__c as Contract_Id,
+        Portfolio_State__c as Portfolio_State,
+        cast(Portfolio_State_Changed__c as date) as Portfolio_State_Changed,
+    from `third-being-207111.RAW.SF_PORTFOLIO`
+    where
+        RecordTypeId != '0127R000000L3HSQA0'
+        and RecordTypeId is not null
+)
+
+select * from portfolio
